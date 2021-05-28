1a3a0f591c93b6af4c104f0525f200d3374c5517
aerrata <aerrata@users.noreply.github.com>
2021-05-28 15:45:37 +0000

    deploy: aerrata/journal@3c7b589507ddd138edcd28d606ee17f0e8c141ef

---
+++ _nuxt/static/1622216736/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622216736",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ebfc89ea"}}}}(null,"\u002F"));
