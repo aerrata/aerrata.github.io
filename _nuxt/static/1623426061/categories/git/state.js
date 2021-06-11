b90b6d7c986acda162aaad562299dd008d3e467e
aerrata <aerrata@users.noreply.github.com>
2021-06-11 15:45:06 +0000

    deploy: aerrata/journal@a89d956c1009e670b4b922448576e1ed71101438

--- _nuxt/static/1623426061/categories/git/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623426061",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcategories\u002Fgit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f9cb672f"}}}}(null));
